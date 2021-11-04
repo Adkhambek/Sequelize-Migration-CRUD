2e0d78bd5b7dce355a98707a8bc019f89d01737f
Adham <muzaffarov.adham@gmail.com>
2021-11-04 06:36:49 +0500

    reset

--- src/server.js
+++ src/server.js
@@ -2,17 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const Models = require("./sequelize/models");
-
-Models.sequelize.sync({
-    force: true,
-    logging: console.log
-}).then(function () {
-    console.log('Nice! Database looks fine')
-
-}).catch(function (err) {
-    console.log(err, "Something went wrong with the Database Update!")
-});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
